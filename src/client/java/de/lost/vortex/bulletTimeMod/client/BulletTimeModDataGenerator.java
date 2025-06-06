8767ce6fdf2004e6a862d43d1a3bdc62e90a3a50
vortexghg <145218229+vortexghg@users.noreply.github.com>
2025-06-06 19:15:20 +0200

    sound problems

--- src/client/java/de/lost/vortex/bulletTimeMod/client/BulletTimeModDataGenerator.java
+++ src/client/java/de/lost/vortex/bulletTimeMod/client/BulletTimeModDataGenerator.java
@@ -1,5 +1,6 @@
 package de.lost.vortex.bulletTimeMod.client;
 
+import de.lost.vortex.bulletTimeMod.sound.ModSound;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
@@ -8,5 +9,6 @@ public class BulletTimeModDataGenerator implements DataGeneratorEntrypoint {
     @Override
     public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
         FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+        ModSound.initialize();
     }
 }
--- src/main/java/de/lost/vortex/bulletTimeMod/BulletTimeMod.java
+++ src/main/java/de/lost/vortex/bulletTimeMod/BulletTimeMod.java
@@ -2,10 +2,6 @@ package de.lost.vortex.bulletTimeMod;
 
 import de.lost.vortex.bulletTimeMod.sound.ModSound;
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
-import net.minecraft.sound.SoundEvent;
-import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,10 +13,6 @@ public class BulletTimeMod implements ModInitializer {
 
     @Override
     public void onInitialize() {
-        Registry.register(Registries.SOUND_EVENT, Identifier.of(MOD_ID, "bullet_time_enter"),
-                SoundEvent.of(Identifier.of(MOD_ID, "bullet_time_enter")));
-        Registry.register(Registries.SOUND_EVENT, Identifier.of(MOD_ID, "bullet_time_leave"),
-                SoundEvent.of(Identifier.of(MOD_ID, "bullet_time_leave")));
         ModSound.initialize();
         TickHandler.register();
     }
