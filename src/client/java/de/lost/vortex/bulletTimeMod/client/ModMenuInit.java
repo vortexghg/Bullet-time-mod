c910b3f7c312ae90053b93ee0eddecc02b2ad521
vortexghg <145218229+vortexghg@users.noreply.github.com>
2025-06-24 16:21:14 +0200

    idk anymore

---
+++ src/client/java/de/lost/vortex/bulletTimeMod/client/ModMenuInit.java
@@ -0,0 +1,14 @@
+package de.lost.vortex.bulletTimeMod.client;
+
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class ModMenuInit implements ModMenuApi {
+    @Override
+    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+        return parent -> null;
+    }
+}
